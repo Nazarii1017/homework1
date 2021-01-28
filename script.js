ff1276febbf494a46291266384d26d7546e1f60f
Nazarii1017 <72093400+Nazarii1017@users.noreply.github.com>
2021-01-28 15:05:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// task1
+
+<script> alert("Nazarii")</script> 
+// task2
+HTML
+<script src="index.js"></script>
+JS
+alert ("Kutepiv")
+// task3
+a) 
+let x;
+let y;
+
+b) 
+let x = 1;
+let y = 2;
+c)
+let dog;
+dog = "black";
+alert(dog);
+
+let cat;
+cat = "white";
+alert(cat);
+
+d)
+let birds = "fly"
+let penguins = birds;
+
+e)
+alert(birds);
+alert(penguins);
+
+// task4
+“String”= let computer = "new"
+
+ “Number”= let computer = 5;
+“Boolean”= Boolean(10<9);
+ “Undefined”= let computer;
+“Null”= let computer = null;
+
+// task5
+let isAdult = confirm("Are you more than 18 y.o.?");
+alert(isAdult);
+isAdult = true;
+console.log(isAdult);
+
+// task6
+let name = "Nazarii"; String
+let surName ="Kutepiv"; String
+let group = 569; Number
+let yearOfBirth = 1998; Number
+let maritalStatus = "unmarried"; String
+
+// task7
+let user = prompt("What is your name?",);
+let email = prompt("What is your email?");
+let password = prompt("What is your password?");
+alert("Dear " + user + " your email is " + email + " and your password is " + password);
+
+// task 8
+let secondsInAMinute = 60;
+let minutesInAnHour = 60;
+let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(econdsInAnHour);
+
+let hoursInADay = 24;
+let secondsInADay = secondsInAnHour * hoursInADay;
+console.log(secondsInADay);
+ 
+let daysInAMonth = 30;
+let secondsInAMonth = daysInAMonth * secondsInADay;
+console.log(secondsInAMonth);
